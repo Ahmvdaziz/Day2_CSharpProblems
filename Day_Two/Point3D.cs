7530c49239e3f668f2d80464fbea96104a1fa3f0
Ahmvd <ahmvdaziz@gmail.com>
2024-08-06 13:01:33 +0300

    Add project files.

---
+++ Day_Two/Point3D.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_Two
+{
+    internal class Point3D
+    {
+        public int x, y, z;
+
+        public Point3D() : this(10, 20, 30)
+        {
+
+        }
+
+        public Point3D(int _x, int _y, int _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+
+        public void displayAll()
+        {
+            Console.WriteLine($"value of x =  {x} \t , value of y =  {y} \t , value of z = {z}");
+        }
+
+        public int CalcDistance(Point3D p1)
+        {
+            int distance;
+            double partx = (int)Math.Pow((x - p1.x), 2);
+            distance = (int)Math.Sqrt(partx);
+            return distance;
+        }
+    }
+}
---
+++ Day_Two/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_Two
+{
+    
+    internal class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            Point3D point3D = new Point3D();
+            
+            Console.WriteLine(point3D.x);
+
+            Fraction f1 = new Fraction();
+
+            Fraction result = f1.add(f1);
+
+            Console.WriteLine($"the Sum = {result.Nom} / {result.Den} ");
+        }
+    }
+   
+}
